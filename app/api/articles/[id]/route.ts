cdf3255c9b1dc008dee6d297a5b39e797ce0dc67
andrewjumperdev <andrew.jumper.ent@gmail.com>
2025-04-04 01:23:47 +0200

    Blog

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -3,21 +3,20 @@ import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
 export async function GET(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
   const client = await clientPromise;
   const db = client.db("blog-andrewcr");
 
   try {
     const article = await db
       .collection("articles")
-      .findOne({ _id: new ObjectId(id) });
+      .findOne({ _id: new ObjectId(context.params.id) });
 
     if (!article) {
       return NextResponse.json({ message: "Artículo no encontrado" }, { status: 404 });
     }
 
     return NextResponse.json(article, { status: 200 });
-  } catch {
+  } catch (error) {
     return NextResponse.json({ message: "Error al obtener el artículo" }, { status: 500 });
   }
 }
