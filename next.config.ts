347d854bd264dac83656cd5779ded06a4e6fce92
andrewjumperdev <andrew.jumper.ent@gmail.com>
2026-03-16 10:08:52 +0100

    Bug:Fix - Redesign UI/UX, Update Info

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['miro.medium.com', 'abcmperformances.com', 'images.pexels.com'],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
 };
 
 export default nextConfig;
